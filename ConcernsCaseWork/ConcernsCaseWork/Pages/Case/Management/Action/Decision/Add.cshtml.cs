486543e4e8e1ae76af93ed37034941b7ad1b33f8
AZIA, Stephane <Stephane.AZIA@EDUCATION.GOV.UK>
2026-04-14 09:12:12 +0100

    When did the department receive the request?

--- ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/Decision/Add.cshtml.cs
+++ ConcernsCaseWork/ConcernsCaseWork/Pages/Case/Management/Action/Decision/Add.cshtml.cs
@@ -436,7 +436,7 @@ namespace ConcernsCaseWork.Pages.Case.Management.Action.Decision
 
 		private static OptionalDateTimeUiComponent BuildReceivedRequestDateComponent(OptionalDateModel? date = null)
 		{
-			return new OptionalDateTimeUiComponent("request-received", nameof(ReceivedRequestDate), "When did SFSO (Schools Financial Support and Oversight) receive the request?")
+			return new OptionalDateTimeUiComponent("request-received", nameof(ReceivedRequestDate), "When did the department receive the request?")
 			{
 				Date = date,
 				DisplayName = "Date SFSO received request"
